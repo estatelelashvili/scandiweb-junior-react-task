4d1090484438804f6979c9ceb9ddb40183accb03
Estate Lelashvili <estatelelashvili@gmail.com>
2022-10-09 00:09:24 +0400

    feat: add navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+export class NavBar extends Component {
+  render() {
+    return (
+      <header>
+        <nav>
+          <ul>
+            <li>
+              <a href='#all'>All</a>
+            </li>
+            <li>
+              <a href='#clothes'>Clothes</a>
+            </li>
+            <li>
+              <a href='#tech'>Tech</a>
+            </li>
+            <li className='active'>
+              <a href='#buy'>Buy</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    );
+  }
+}
