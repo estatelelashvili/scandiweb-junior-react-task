09b87faac46fe214aa0e529e31304120c1af73ae
Estate Lelashvili <estatelelashvili@gmail.com>
2022-10-09 01:18:23 +0400

    refactor: update App as a class component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import './index.css';
 import { NavBar } from './components/Navbar';
 import { PLP } from './components/PLP';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div>
-      <NavBar />
-      <PLP />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <NavBar />
+        <PLP />
+      </div>
+    );
+  }
 }
 
 export default App;
